e24138dd0495b53251e0a43ba7e864e90eb509b9
Tymoteusz Czech <mail@tymek.cz>
2017-10-29 02:43:05 +0200

    Simple move

--- scripts.js
+++ scripts.js
@@ -12,7 +12,25 @@ $(function() {
                 color = 'white';
             }
             $("#game").find('.row').last()
-                .append('<div id="' + id + '" class="' + color + '">' + id + '</div>');
+                .append('<div data-id="' + id + '" class="' + color + '">' + id + '</div>');
         }
     }
+    var white = [3, 3];
+    $('.row div').on('click', function(e) {
+        var t = $(this);
+        if (t.hasClass('white')) return;
+        var color;
+        if (t.hasClass('blue')) {
+            color = 'blue'
+        } else if (t.hasClass('green')) {
+            color = 'green';
+        }
+        var block = t.attr('data-id').split('-');
+        var x = Math.sqrt(Math.pow(white[0]-block[0], 2) + Math.pow(white[1]-block[1], 2));
+        if (x <= 1) {
+            $('[data-id=' + white.join('-') + ']').removeClass('white').addClass(color);
+            white = block;
+            $('[data-id=' + block.join('-') + ']').removeClass(color).addClass('white');
+        }
+    });
 });
